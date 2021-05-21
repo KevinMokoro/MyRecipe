9fa0427fa6172c5a4591ad9886148764499efe8d
KevinMokoro <klosvoke1@gmail.com>
2021-05-21 14:34:55 +0300

    This will add a custom adapter to display both the recie name and its source.

--- app/src/main/java/com/moringaschool/myrecipe/RecipesActivity.java
+++ app/src/main/java/com/moringaschool/myrecipe/RecipesActivity.java
@@ -2,7 +2,6 @@ package com.moringaschool.myrecipe;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,6 +15,8 @@ public class RecipesActivity extends AppCompatActivity {
     @BindView(R.id.listViewRecipes) ListView mListViewRecipes;
     String[] recipes = new String[] { "brown rice","hum","hay","spaghetti","veet",
             "chickenMasala","viazi karai","uji","brown rice","mushroom","soup","yum","rum","choma","githeri"};
+    String[] sources = new String[] { "Kevin Cafe", "Chef Kevin", "Taita Cafe", "BIG", "Elna", "Naden", "AIden", "Just",
+    "KIki", "Jackiana", "Wakesho", "Suswa", "Box", "JUliet Towers", "Sixtus"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +24,7 @@ public class RecipesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipes);
         ButterKnife.bind(this);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, recipes);
+        MyRecipesAdapter adapter = new MyRecipesAdapter(this, android.R.layout.simple_list_item_1, recipes, sources);
         mListViewRecipes.setAdapter(adapter);
 
         Intent intent = getIntent();
