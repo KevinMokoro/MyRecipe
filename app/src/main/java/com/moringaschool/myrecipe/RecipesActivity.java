2800b791bca712c7e3b07f4649aa5c2d630aac12
KevinMokoro <klosvoke1@gmail.com>
2021-05-21 15:12:39 +0300

    This will add a toast to recipes activity to show a clicked item on the list.

--- app/src/main/java/com/moringaschool/myrecipe/RecipesActivity.java
+++ app/src/main/java/com/moringaschool/myrecipe/RecipesActivity.java
@@ -2,8 +2,11 @@ package com.moringaschool.myrecipe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,12 +30,15 @@ public class RecipesActivity extends AppCompatActivity {
         MyRecipesAdapter adapter = new MyRecipesAdapter(this, android.R.layout.simple_list_item_1, recipes, sources);
         mListViewRecipes.setAdapter(adapter);
 
+        mListViewRecipes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?>parent, View view, int position, long l) {
+                String recipe = ((TextView)view).getText().toString();
+                Toast.makeText(RecipesActivity.this, recipe, Toast.LENGTH_LONG).show();
+            }
+        });
         Intent intent = getIntent();
         String ingredient = intent.getStringExtra("ingredient");
         mIngredientTextView.setText("Ready Recipes That Match Your Ingredient: " + ingredient);
-
-
     }
-
-
 }
