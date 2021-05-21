7ecb94aaf9935752d468aa3b5a30e7e3d3e8ff54
KevinMokoro <klosvoke1@gmail.com>
2021-05-21 05:50:21 +0300

    This will bind views in Recipes activities for later use.

--- app/src/main/java/com/moringaschool/myrecipe/RecipesActivity.java
+++ app/src/main/java/com/moringaschool/myrecipe/RecipesActivity.java
@@ -1,14 +1,26 @@
 package com.moringaschool.myrecipe;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RecipesActivity extends AppCompatActivity {
+    @BindView(R.id.ingredientTextView) TextView mIngredientTextView;
+    @BindView(R.id.listView) ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
+        ButterKnife.bind(this);
+
+
     }
+
+
 }
