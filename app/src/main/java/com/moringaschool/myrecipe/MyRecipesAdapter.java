0e5b96d947449ca7ec9e52ba531555ff2b3d8391
KevinMokoro <klosvoke1@gmail.com>
2021-05-21 06:38:54 +0300

    Add an adapter to diplay a list of an array of recipes.

---
+++ app/src/main/java/com/moringaschool/myrecipe/MyRecipesAdapter.java
@@ -0,0 +1,5 @@
+package com.moringaschool.myrecipe;
+
+public class MyRecipesAdapter {
+
+}
--- app/src/main/java/com/moringaschool/myrecipe/RecipesActivity.java
+++ app/src/main/java/com/moringaschool/myrecipe/RecipesActivity.java
@@ -1,6 +1,8 @@
 package com.moringaschool.myrecipe;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -11,13 +13,22 @@ import butterknife.ButterKnife;
 
 public class RecipesActivity extends AppCompatActivity {
     @BindView(R.id.ingredientTextView) TextView mIngredientTextView;
-    @BindView(R.id.listView) ListView mListView;
+    @BindView(R.id.listViewRecipes) ListView mListViewRecipes;
+    String[] recipes = new String[] { "brown rice","hum","hay","spaghetti","veet",
+            "chickenMasala","viazi karai","uji","brown rice","mushroom","soup","yum","rum","choma","githeri"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
         ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, recipes);
+        mListViewRecipes.setAdapter(adapter);
+
+        Intent intent = getIntent();
+        String ingredient = intent.getStringExtra("ingredient");
+        mIngredientTextView.setText("Ready Recipes That Match Your Ingredient: " + ingredient);
 
 
     }
