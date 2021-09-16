19c82ac2dfc766e6d20168e4db227af0ffb8c930
KevinMokoro <klosvoke1@gmail.com>
2021-09-16 10:17:34 +0300

    This will add a fragment for details of a specific recipe.

---
+++ app/src/main/java/com/moringaschool/myrecipe/ui/RecipeDetailActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.myrecipe.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.moringaschool.myrecipe.R;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+    }
+}
