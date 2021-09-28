df77fc2ff9e6bd4faebcd33c7d3e98c1513787ce
KevinMokoro <klosvoke1@gmail.com>
2021-09-28 16:45:25 +0300

    This will add a signup and login activity plus their corresponding layouts.

---
+++ app/src/main/java/com/moringaschool/myrecipe/ui/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.myrecipe.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.moringaschool.myrecipe.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
